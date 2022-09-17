5f3582f10d5d5cc43de0dfb315ea9bc4cc4e5bcf
DiptoChakrabarty <diptochuck123@gmail.com>
2022-09-17 11:50:51 +0530

    restructing slack client

--- notif/send-basic-msg.go
+++ notif/send-basic-msg.go
@@ -10,6 +10,36 @@ import (
 	"github.com/slack-go/slack"
 )
 
+type SlackModel struct {
+	Connection *slack.Client
+}
+
+func loadEnv(key, defaultValue string) string {
+	err := godotenv.Load(".env")
+	if err != nil {
+		logger.Error("Some error occured: %s", err)
+		logger.Info("Defaulting to default values")
+		return defaultValue
+	}
+	if value, ok := os.LookupEnv(key); ok {
+		return value
+	}
+	return defaultValue
+}
+
+func getSlackClient() *slack.Client {
+	oauthToken := loadEnv("OAUTH_TOKEN", "somth")
+	return slack.New(oauthToken)
+
+}
+
+func NewSlackClient() *SlackModel {
+	client := getSlackClient()
+	return &SlackModel{
+		Connection: client,
+	}
+}
+
 func getConnection() {
 	err := godotenv.Load(".env")
 	if err != nil {
