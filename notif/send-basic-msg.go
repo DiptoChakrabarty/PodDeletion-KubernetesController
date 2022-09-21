29b7f79f64095e1b7e69d86e7414e201014a8a93
DiptoChakrabarty <diptochuck123@gmail.com>
2022-09-21 21:20:15 +0530

    add yamls and logs

--- notif/send-basic-msg.go
+++ notif/send-basic-msg.go
@@ -1,8 +1,6 @@
 package notif
 
 import (
-	"fmt"
-	"log"
 	"os"
 
 	"github.com/DiptoChakrabarty/podDeletionController/logger"
@@ -42,29 +40,8 @@ func NewSlackClient() *SlackModel {
 
 func (client *SlackModel) SendMessage(s string) (string, string, error) {
 	channelID, timestamp, err := client.Connection.PostMessage(
-		loadEnv("CHANNEL_ID"),
-		slack.MsgOptionText(s, false)
+		loadEnv("CHANNEL_ID", "not provided"),
+		slack.MsgOptionText(s, false),
 	)
 	return channelID, timestamp, err
 }
-
-/* func getConnection() {
-	err := godotenv.Load(".env")
-	if err != nil {
-		log.Fatalf("Some error occured: %s", err)
-	}
-	api := slack.New(os.Getenv("OAUTH_TOKEN"))
-	channelID, timestamp, err := api.PostMessage(
-		os.Getenv("CHANNEL_ID"),
-		slack.MsgOptionText("Hello World", false),
-	)
-
-	if err != nil {
-		logger.Error("Some error occured: %s", err)
-	}
-	fmt.Println("Message sent successfully at %s to channel ID %s", timestamp, channelID)
-}
-
-func main() {
-	getConnection()
-} */
